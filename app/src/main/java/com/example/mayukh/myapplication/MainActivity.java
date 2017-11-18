03c50ec84cf4d080bbaae13ce900411ebf5ff200
mayukh45 <mayukh1110@gmail.com>
2017-11-18 15:09:47 +0530

    version 2.2

--- app/src/main/java/com/example/mayukh/myapplication/MainActivity.java
+++ app/src/main/java/com/example/mayukh/myapplication/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.mayukh.myapplication;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -12,47 +14,58 @@ import android.widget.Toast;
 import java.math.BigInteger;
 
 public class MainActivity extends AppCompatActivity {
-private EditText ed;
-    private EditText edrad;
-private TextView tv;
+    private EditText ed;
+    private EditText rad;
+    private EditText nrad;
+    private TextView tv;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //init();
+
     }
 
 
-    public void increment()
-    {
-        tv=(TextView) (findViewById(R.id.tv1));
-        ed=(EditText)(findViewById(R.id.ed1));
-        edrad=(EditText)(findViewById(R.id.ed2));
+    public void increment() {
+        tv = (findViewById(R.id.tv1));
+        ed = (findViewById(R.id.num));
+        rad = (findViewById(R.id.rad1));
+        nrad = (findViewById(R.id.rad2));
+
+        //return types have been changed in effect from android studio 3.0
+        // now they are automatically typecasted to required datatype
+        //cheers!
         BigInteger bi;
-        int n=0;
+        int n = 0,nn=0;
 
         try {
-             bi = new BigInteger(ed.getText().toString().trim());
-             n = Integer.parseInt(edrad.getText().toString().trim());
-            if(n>36){
-                Toast.makeText(this, "Maximum Base can be 36", Toast.LENGTH_SHORT).show();
+            n = Integer.parseInt(rad.getText().toString().trim());
+            bi = new BigInteger(ed.getText().toString().trim(),n);
+
+            if (n > 36) {
+                Toast.makeText(this, "Base is always <=36", Toast.LENGTH_SHORT).show();
                 return;
             }
-        }catch(Exception e){
-            Toast.makeText(this, "DONT BE OVERSMART!", Toast.LENGTH_SHORT).show();
+        } catch (Exception e) {
+            Toast.makeText(this, "Incorrect Input", Toast.LENGTH_SHORT).show();
             return;
         }
-        String s=bi.toString(n);
+        nn=Integer.parseInt(nrad.getText().toString().trim());
+
+        if (nn > 36) {
+            Toast.makeText(this, "Base is always <=36", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        String s = bi.toString(nn);
         tv.setText(s);
         Toast.makeText(this, "Thanks For Using :)", Toast.LENGTH_SHORT).show();
     }
 
-    public void ho(View view)
-    {
+    public void ho(View view) {
 
-increment();
-    }
-    public void hoo(View view) {
         increment();
     }
 }
+
